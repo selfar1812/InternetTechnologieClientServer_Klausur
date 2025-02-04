19b162701a7431a99db64c3a466269e56003c1f5
Shelly Farhani <shellyfarhani@Shellys-MacBook-Pro.local>
2025-02-04 17:34:42 +0100

    test2

--- graph.php
+++ graph.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root"; 
 $password = "root"; 
-$dbname = "fel_data"; 
+$dbname = "fel_data"; //datenbankname
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
